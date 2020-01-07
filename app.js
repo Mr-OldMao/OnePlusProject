f2292a075098d0dab2fc84ee586f89a988392511
Mao <929764654@qq.com>
2020-01-07 15:03:44 +0800

    简单的登录相应测试
    给定用户名zhangsan为 已被占用用户来做测试
    
    url: localhost:3001/login.html

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 var http = require("http");
 var fs = require("fs"); //文件系统的模块
 var server = http.createServer();
-server.on("request", function(req, res) 
-{
-	console.log(req.method);
-	//请求的方式
-	console.log(req.url);
-	//请求的地址  
-			if (req.url == "/page/index.html" && req.method == "GET") 
-			{ 
-				fs.readFile("./page/index.html", "utf8", function(err, data) {
-					res.end(data);
-				});
-			} else if (req.url == "/page/login.html" && req.method == "GET") 
-			{ 
-				fs.readFile("./page/login.html", "utf8", function(err, data) {
-					res.end(data);
-				});
-			}else if(req.url == "/getData" && req.method == "GET"){
-				console.log('已经拿到数据')
-				res.end('拿到了app中数据');
-			}
-			 else 
-			{
-				fs.readFile("./page/404.html", "utf8", function(err, data) {
-					res.end(data);
-				});
-			}
-}); 
+var url = require("url")
+
+server.on("request", function(req, res) {
+	/* 	console.log(req.method);
+		//请求的方式
+		console.log(req.url);
+		//请求的地址  */
+
+
+	var pathname = url.parse(req.url, true).pathname; //获取地址
+	var query = url.parse(req.url, true).query; //获取参数
+	console.log(pathname, query.username)
+
+	if (req.url == "/index.html" && req.method == "GET") {
+		fs.readFile("./index.html", "utf8", function(err, data) {
+			res.end(data);
+		});
+	} else if (req.url == "/login.html" && req.method == "GET") {
+		fs.readFile("./login.html", "utf8", function(err, data) {
+			res.end(data);
+		});
+	} else if (pathname == "/regusername" && req.method == "GET") {
+		var username = query.username;
+		console.log(username);
+		if (username == "zhangsan") {
+			res.end("0")
+		} else {
+			res.end("1")
+		}
+	}
+});
 server.listen(3001);
